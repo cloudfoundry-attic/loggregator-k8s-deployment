5238f23e397cc2d0672d4aa6fa6848f4a45f9c0a
Jason Keene <jkeene@pivotal.io>
2017-11-29 13:47:06 -0700

    Add scripts to create and destroy all the objects

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+ls secrets/*.yml | while read line; do
+    kubectl apply -f "$line"
+done
+
+ls services/*.yml | while read line; do
+    kubectl apply -f "$line"
+done
+
+ls deployments/*.yml | while read line; do
+    kubectl apply -f "$line"
+done
---
+++ destroy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+ls deployments/*.yml | while read line; do
+    kubectl delete -f "$line"
+done
+
+ls services/*.yml | while read line; do
+    kubectl delete -f "$line"
+done
+
+ls secrets/*.yml | while read line; do
+    kubectl delete -f "$line"
+done

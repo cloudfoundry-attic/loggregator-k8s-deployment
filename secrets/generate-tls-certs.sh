5d9163012a2b5aaf789540c65303e61e009b93c8
Jason Keene <jkeene@pivotal.io>
2017-11-29 12:49:04 -0700

    Initial deployment with ClusterIP router discovery

---
+++ secrets/generate-tls-certs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+tls_certs_header='
+apiVersion: v1
+kind: Secret
+metadata:
+  name: loggregator-tls-certs
+type: Opaque
+data:
+'
+
+docker run -v "$PWD/loggregator-tls-certs:/output" loggregator/certs
+echo "$tls_certs_header" > loggregator-tls-certs.yml
+ls loggregator-tls-certs |
+    while read line
+        do echo "  $line: $(cat loggregator-tls-certs/$line | base64)"
+    done >> loggregator-tls-certs.yml

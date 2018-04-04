87926f27c34b171e65b99557b1b0525d040c55ba
Jason Keene <jkeene@pivotal.io>
2018-04-04 12:46:17 -0600

    Update script to chown certs and base64 encode correctly
    Signed-off-by: Warren Fernandes <wfernandes@pivotal.io>

--- secrets/generate-tls-certs.sh
+++ secrets/generate-tls-certs.sh
@@ -13,8 +13,9 @@ data:
 '
 
 docker run -v "$PWD/loggregator-tls-certs:/output" loggregator/certs
+sudo chown -R "$(whoami):$(whoami)" loggregator-tls-certs
 echo "$tls_certs_header" > loggregator-tls-certs.yml
 ls loggregator-tls-certs |
     while read line
-        do echo "  $line: $(cat loggregator-tls-certs/$line | base64)"
+        do echo "  $line: $(cat loggregator-tls-certs/$line | base64 -w 0)"
     done >> loggregator-tls-certs.yml

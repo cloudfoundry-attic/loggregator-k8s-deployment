38a165274e45e413bfe07200026a31af2fbdd028
Jason Keene <jkeene@pivotal.io>
2018-04-25 10:39:46 -0600

    Get secret generation script running on OSX again

--- secrets/generate-tls-certs.sh
+++ secrets/generate-tls-certs.sh
@@ -12,10 +12,15 @@ type: Opaque
 data:
 '
 
+b64cmd=base64
+if [ "$(uname | tr A-Z a-z)" = "linux" ]; then
+  b64cmd="base64 -w 0"
+fi
+
 docker run -v "$PWD/loggregator-tls-certs:/output" loggregator/certs
-sudo chown -R "$(whoami):$(whoami)" loggregator-tls-certs
+sudo chown -R "$(whoami)" loggregator-tls-certs
 echo "$tls_certs_header" > loggregator-tls-certs.yml
 ls loggregator-tls-certs |
     while read line
-        do echo "  $line: $(cat loggregator-tls-certs/$line | base64 -w 0)"
+        do echo "  $line: $(cat loggregator-tls-certs/$line | $b64cmd)"
     done >> loggregator-tls-certs.yml

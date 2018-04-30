dbc2308b5b9c52d7c65fcdf016cea308edcbbcf7
Jason Keene <jkeene@pivotal.io>
2018-04-30 13:31:06 -0600

    Only do chown on linux

--- secrets/generate-tls-certs.sh
+++ secrets/generate-tls-certs.sh
@@ -18,7 +18,9 @@ if [ "$(uname | tr A-Z a-z)" = "linux" ]; then
 fi
 
 docker run -v "$PWD/loggregator-tls-certs:/output" loggregator/certs
-sudo chown -R "$(whoami)" loggregator-tls-certs
+if [ "$(uname | tr A-Z a-z)" = "linux" ]; then
+  sudo chown -R "$(whoami)" loggregator-tls-certs
+fi
 echo "$tls_certs_header" > loggregator-tls-certs.yml
 ls loggregator-tls-certs |
     while read line

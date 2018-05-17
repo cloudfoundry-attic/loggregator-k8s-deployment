eadf4e0abb1f603d7b10ee527ccd8f235c9913b4
Warren Fernandes <wfernandes@pivotal.io>
2018-05-17 10:50:17 -0600

    Roll components on deploy
    [#157666802]
    
    Signed-off-by: Jason Keene <jkeene@pivotal.io>

--- deploy.sh
+++ deploy.sh
@@ -17,4 +17,19 @@ function single_yml_config {
     done
 }
 
+function patch_objects {
+    # this is done in order to force rolling of components on update
+    # for instance if a configmap or secret value is changed and needs to be
+    # reloaded from disk or env
+    patch='{"spec": {"template": {"metadata": {"labels": {"randomversion": "'$RANDOM'"}}}}}'
+    kubectl patch statefulset log-cache --namespace loggregator --patch "$patch"
+    kubectl patch deployment log-cache-nozzle --namespace loggregator --patch "$patch"
+    kubectl patch deployment log-cache-scheduler --namespace loggregator --patch "$patch"
+    kubectl patch deployment loggregator-emitter --namespace loggregator --patch "$patch"
+    kubectl patch deployment loggregator-rlp --namespace loggregator --patch "$patch"
+    kubectl patch deployment loggregator-router --namespace loggregator --patch "$patch"
+    kubectl patch daemonset loggregator-fluentd --namespace loggregator --patch "$patch"
+}
+
 single_yml_config | kubectl apply -f -
+patch_objects
